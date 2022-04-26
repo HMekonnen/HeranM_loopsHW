7939ad63f81c9df8f4950bd7d243f9086bdea0b8
HMekonnen <93164313+HMekonnen@users.noreply.github.com>
2022-04-26 01:10:38 -0400

    Add files via upload

---
+++ radioObjecct.js
@@ -0,0 +1,15 @@
+let stations = {
+    station:{  
+        name: "123",
+        songs:" ",
+              
+              title: "",
+              artist: " ",
+              
+             changeStation(){
+          station = [Math.floor(Math.random()*(5-1)+1),Math.floor(Math.random()*(5-1)+1),Math.floor(Math.random()*(5-1)+1)]
+       console.log(`Now playing: ${song.title} by ${song.artist}`)}}}
+        
+        
+    
+    
